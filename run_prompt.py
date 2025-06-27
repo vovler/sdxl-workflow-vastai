bf11110a571272f6da0404632b4a909f95879c87
DigitalCart-FZCO <team@digital-cart.co>
2025-06-27 13:41:10 +0100

    Adjust num_steps to 6 in generate_image function of run_prompt.py for optimized image generation performance.

--- run_prompt.py
+++ run_prompt.py
@@ -68,7 +68,7 @@ def generate_image(
 
     width = 1152
     height = 768
-    num_steps = 8 # DMD2 is optimized for 4 steps
+    num_steps = 6 # DMD2 is optimized for 4 steps
     guidance = 1.2
 
     out = pipe(

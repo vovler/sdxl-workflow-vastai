cc3ebb7687e7d4c40577effbd8915f0c193aebfc
DigitalCart-FZCO <team@digital-cart.co>
2025-07-02 12:48:49 +0100

    Update ONNX model paths in defaults.py for VAE and text encoders to use optimized models, enhancing compatibility and performance.

--- pipeline/defaults.py
+++ pipeline/defaults.py
@@ -10,11 +10,11 @@ DEFAULT_BASE_MODEL = "socks22/sdxl-wai-nsfw-illustriousv14"
 ONNX_MODELS_DIR = "/workflow/wai_dmd2_onnx"
 
 # VAE
-VAE_DECODER_PATH = os.path.join(ONNX_MODELS_DIR, "tiny_vae_decoder", "model_l.onnx")
+VAE_DECODER_PATH = os.path.join(ONNX_MODELS_DIR, "tiny_vae_decoder", "model.onnx")
 
 # UNet
 UNET_PATH = os.path.join(ONNX_MODELS_DIR, "unet", "model_opt.onnx")
 
 # Text Encoders
-CLIP_TEXT_ENCODER_1_PATH = os.path.join(ONNX_MODELS_DIR, "text_encoder", "model.onnx")
-CLIP_TEXT_ENCODER_2_PATH = os.path.join(ONNX_MODELS_DIR, "text_encoder_2", "model.onnx") 
+CLIP_TEXT_ENCODER_1_PATH = os.path.join(ONNX_MODELS_DIR, "text_encoder", "model_opt.onnx")
+CLIP_TEXT_ENCODER_2_PATH = os.path.join(ONNX_MODELS_DIR, "text_encoder_2", "model_opt.onnx") 

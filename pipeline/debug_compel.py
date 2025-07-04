8e178fb84cec2adc0e18ce55ddac9e9866beb057
DigitalCart-FZCO <team@digital-cart.co>
2025-07-04 02:01:57 +0100

    Update prompt in debug_compel.py to escape parentheses in 'aqua_(konosuba)' for improved compatibility in scene generation.

--- pipeline/debug_compel.py
+++ pipeline/debug_compel.py
@@ -12,7 +12,7 @@ import modelopt.torch.opt as mto
 
 
 # Ensure you use the exact same settings as your pipeline
-prompt = "masterpiece, best quality, amazing quality, very aesthetic, high resolution, ultra-detailed, absurdres, newest, scenery, wholesome, night, 1girl, aqua_\(konosuba\), anal sex, ahegao, heart shaped eyes"
+prompt = "masterpiece, best quality, amazing quality, very aesthetic, high resolution, ultra-detailed, absurdres, newest, scenery, wholesome, night, 1girl, aqua_\\(konosuba\\), anal sex, ahegao, heart shaped eyes"
 height = 1024
 width = 1024
 num_inference_steps = 8

Update latent scaling in SDXLPipeline to use a fixed value for consistent VAE processing

pipeline_rt/pipeline.py
@@ -158,7 +158,7 @@ class SDXLPipeline:
         vae_start_time = time.time()
         
         # Manually scaling latents for all VAEs for consistent debugging
-        scaled_latents = latents / self.vae_scale_factor
+        scaled_latents = latents / 0.13025
         image_np = self.vae(scaled_latents.to(torch.float16))
 
         vae_end_time = time.time()
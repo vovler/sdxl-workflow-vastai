c7e6a5f7bc7210b139ff78a8623f44e1e934377e
DigitalCart-FZCO <team@digital-cart.co>
2025-07-07 20:15:24 +0100

    Update VAE loading in loader.py to include configuration path
    - Added a configuration path parameter to the instantiation of the alternative VAE model, enhancing flexibility in loading configurations.

--- pipeline_rt/loader.py
+++ pipeline_rt/loader.py
@@ -32,7 +32,8 @@ def load_pipeline_components():
 
     vae_alt = AutoencoderKL.from_single_file(
         defaults.VAE_ALT_PATH,
-        dtype=torch.float16
+        dtype=torch.float16,
+        config=vae_alt_config_path
     ).to(device)
     vae_alt.enable_xformers_memory_efficient_attention()
     vae_alt.enable_tiling()

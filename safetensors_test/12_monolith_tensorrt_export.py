584bf9ca46e1f00290d859ab412a1262ca413838
vovler <vovler.lupu1@gmail.com>
2025-07-11 02:16:46 +0100

    Add TQDM progress monitoring for TensorRT engine building in 12_monolith_tensorrt_export.py
    This update introduces a TQDMProgressMonitor class to provide visual feedback during the engine building process. The monitor tracks the progress of different phases and steps, enhancing user experience by displaying real-time progress updates.

--- safetensors_test/12_monolith_tensorrt_export.py
+++ safetensors_test/12_monolith_tensorrt_export.py
@@ -5,6 +5,74 @@ import argparse
 import os
 import sys
 from collections import OrderedDict
+from tqdm import tqdm
+
+class TQDMProgressMonitor(trt.IProgressMonitor):
+    def __init__(self):
+        trt.IProgressMonitor.__init__(self)
+        self._active_phases = {}
+        self._step_result = True
+        self.max_indent = 5
+
+    def phase_start(self, phase_name, parent_phase, num_steps):
+        leave = False
+        try:
+            if parent_phase is not None:
+                nbIndents = (
+                    self._active_phases.get(parent_phase, {}).get(
+                        "nbIndents", self.max_indent
+                    )
+                    + 1
+                )
+                if nbIndents >= self.max_indent:
+                    return
+            else:
+                nbIndents = 0
+                leave = True
+            self._active_phases[phase_name] = {
+                "tq": tqdm(
+                    total=num_steps, desc=phase_name, leave=leave, position=nbIndents
+                ),
+                "nbIndents": nbIndents,
+                "parent_phase": parent_phase,
+            }
+        except KeyboardInterrupt:
+            self._step_result = False
+
+    def phase_finish(self, phase_name):
+        try:
+            if phase_name in self._active_phases.keys():
+                self._active_phases[phase_name]["tq"].update(
+                    self._active_phases[phase_name]["tq"].total
+                    - self._active_phases[phase_name]["tq"].n
+                )
+                parent_phase = self._active_phases[phase_name].get("parent_phase", None)
+                while parent_phase is not None:
+                    self._active_phases[parent_phase]["tq"].refresh()
+                    parent_phase = self._active_phases[parent_phase].get(
+                        "parent_phase", None
+                    )
+                if (
+                    self._active_phases[phase_name]["parent_phase"]
+                    in self._active_phases.keys()
+                ):
+                    self._active_phases[
+                        self._active_phases[phase_name]["parent_phase"]
+                    ]["tq"].refresh()
+                del self._active_phases[phase_name]
+            pass
+        except KeyboardInterrupt:
+            self._step_result = False
+
+    def step_complete(self, phase_name, step):
+        try:
+            if phase_name in self._active_phases.keys():
+                self._active_phases[phase_name]["tq"].update(
+                    step - self._active_phases[phase_name]["tq"].n
+                )
+            return self._step_result
+        except KeyboardInterrupt:
+            return False
 
 def build_engine(
     engine_path: str,
@@ -53,6 +121,8 @@ def build_engine(
         profile.set_shape(name, min=min_shape, opt=opt_shape, max=max_shape)
     
     config.add_optimization_profile(profile)
+
+    config.progress_monitor = TQDMProgressMonitor()
 
     # --- Create Network & Parse ONNX ---
     EXPLICIT_BATCH = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)

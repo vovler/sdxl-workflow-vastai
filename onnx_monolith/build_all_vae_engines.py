41678d1f05858f165b74cc3911a3116672858228
vovler <vovler.lupu1@gmail.com>
2025-07-15 19:39:59 +0000

    Update ONNX files in build_all_vae_engines.py to replace tiled_decoder_optimized_onnxslim_full.onnx with vae_clean.onnx, streamlining engine building options.

--- onnx_monolith/build_all_vae_engines.py
+++ onnx_monolith/build_all_vae_engines.py
@@ -176,7 +176,8 @@ def main():
     
     onnx_files = [
         "decoder_opt.onnx",
-        "tiled_decoder_optimized_onnxslim_full.onnx",
+        "vae_clean.onnx"
+        #"tiled_decoder_optimized_onnxslim_full.onnx",
         #"tiled_decoder_optimized_constant_folding.onnx",
         #"tiled_decoder_optimized_runtime_extended.onnx"
     ]

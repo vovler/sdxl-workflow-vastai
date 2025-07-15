a42c17c83c4ef4f2953d6fc8699552bcd8d8bd59
vovler <vovler.lupu1@gmail.com>
2025-07-15 22:42:17 +0000

    Add blend_v and blend_h methods to AutoEncoderKL class in vae.py for enhanced blending functionality
    This update introduces two new methods, blend_v and blend_h, which allow for vertical and horizontal blending of tensors, respectively. These methods enhance the model's capability to manipulate tensor data during the decoding process.

--- testt/vae.py
+++ testt/vae.py
@@ -320,6 +320,19 @@ class AutoEncoderKL(nn.Module):
         z = self.post_quant_conv(z)
         return self.decoder(z)
 
+
+    def blend_v(self, a: torch.Tensor, b: torch.Tensor, blend_extent: int) -> torch.Tensor:
+        blend_extent = min(a.shape[2], b.shape[2], blend_extent)
+        for y in range(blend_extent):
+            b[:, :, y, :] = a[:, :, -blend_extent + y, :] * (1 - y / blend_extent) + b[:, :, y, :] * (y / blend_extent)
+        return b
+
+    def blend_h(self, a: torch.Tensor, b: torch.Tensor, blend_extent: int) -> torch.Tensor:
+        blend_extent = min(a.shape[3], b.shape[3], blend_extent)
+        for x in range(blend_extent):
+            b[:, :, :, x] = a[:, :, :, -blend_extent + x] * (1 - x / blend_extent) + b[:, :, :, x] * (x / blend_extent)
+        return b
+
     def tiled_decode(self, latent, tile_latent_min_size, tile_sample_min_size, tile_overlap_factor):
         r"""
         Decode a batch of images using a tiled decoder.
